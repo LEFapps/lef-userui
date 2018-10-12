825c0f54d812f404e37f3978404da2345a9f43c4
Johan Beckers <jb@eenwereldmetlef.be>
2018-10-12 20:29:12 +0200

    Fix Dependency

--- package.js
+++ package.js
@@ -1,12 +1,18 @@
 Package.describe({
   summary: 'Simple UI elements for user login/logout',
-  version: '1.0.3',
+  version: '1.0.4',
   name: 'lef:userui',
   git: 'https://github.com/LEFapps/lef-userui'
 })
 
 Package.onUse(api => {
-  api.use(['ecmascript', 'lef:translations', 'lef:alerts', 'react-meteor-data'])
+  api.use([
+    'ecmascript',
+    'lef:translations',
+    'lef:alerts',
+    'lef:systemmailing',
+    'react-meteor-data'
+  ])
   api.addFiles('server.js', 'server')
   api.mainModule('client.js', 'client')
 })
